7bc2350297493bff175280919c94962f5e041856
IamNuke <alexeimartynyuk@gmail.com>
2023-01-05 19:43:01 +0300

    Add solution

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] ProcessArray(String[] strings)
+{
+    String[] result = new String[strings.Length];
+    for (int i = 0; i < strings.GetLength(0); i++)
+    {
+        int randomLength = Math.Min(new Random().Next(0,4), strings[i].Length);
+        for (int j = 0; j < randomLength; j++)
+            result[i] += strings[i][j];
+            
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Enter the strings, separated by a space: ");
+String[] strings = {"asdfasdf","12341234","/*-/*--/*-","kl;kl;","ffffffffff", "555555555555"};//Console.ReadLine().Split(" ");
+
+Console.WriteLine($"Result :[{string.Join("][", ProcessArray(strings))}]");
